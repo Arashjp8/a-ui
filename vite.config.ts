9037557200c359b1a28668bfcc971493cf64fddc
arashjp8 <arashjafarpour723@gmail.com>
2025-05-22 09:32:51 +0330

    feat: types added

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    plugins: [react(), tsconfigPaths(), tailwindcss(), dts({ rollupTypes: true })],
     build: {
         // library entry and output settings
         lib: {
